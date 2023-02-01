f42fa97d545ce1cff288ec365ea9bfa581459af1
egeniyz <egeniyz@ukr.net>
2023-02-01 17:31:44 +0200

    Створити змінні. Вивести кожну змінну. console.log

---
+++ lesson1/main.js
@@ -0,0 +1,28 @@
+let visit = 'hello';
+console.log(visit);
+let sity = 'owu'
+console.log(sity);
+let domen = 'com'
+console.log(domen);
+let country = 'ua'
+console.log(country);
+let one = 1
+console.log(one);
+let ten = 10
+console.log(ten);
+let minus = -999
+console.log(minus);
+let hundr = 123
+console.log(hundr);
+let pi = 3.14
+console.log(pi);
+let fractional = 2.7
+console.log(fractional);
+let sixt = 16
+console.log(sixt);
+let bool1 = true
+console.log(bool1);
+let bool2 = false
+console.log(bool2);
+
+
