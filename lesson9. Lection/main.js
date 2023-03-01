78c8f0b0386682c8055867de79abb1fa39952e3f
egeniyz <egeniyz@ukr.net>
2023-03-01 17:50:15 +0200

    lesson9

---
+++ lesson9. Lection/main.js
@@ -0,0 +1,9 @@
+// console.log(document)       // Выводит весь html документ
+// console.log(document.head)
+// console.log(document.head.innerHTML)
+
+let ul1 = document.getElementById('list-1');
+console.log(ul1)
+
+
+
